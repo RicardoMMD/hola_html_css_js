b786209e6544820fb621eb47264e94f92ec4be18
RicardoMMD <ricardommd.rmd@gmail.com>
2023-02-11 16:12:55 -0600

    Modificaciones al filtro JS mara ser multicategoria

--- js/script.js
+++ js/script.js
@@ -3,35 +3,45 @@
 
 let activeBtn = null;
 
-function filtroCards(category) {
-    let cards = document.querySelectorAll(".card");
-    cards.forEach(
-        card => {
-            if(card.dataset.category === category || category === "all"){
-                card.style.display = "block";
-            }else {
-                card.style.display = "none";
-            }
+function filterCards(selectedCategories) {
+    // select cards
+    const cards = document.querySelectorAll(".card");
+    // loop through each card
+    for (let card of cards) {
+        // get the card's categories
+        let cardCategories = JSON.parse(card.dataset.categories);
+
+        // check if the selected categories overlap with the card's categories
+        let overlap = selectedCategories.some(selectedCategory => cardCategories.includes(selectedCategory));
+        
+        // show the card if there is an overlap or if "all" is selected
+        if (overlap || selectedCategories.includes("all")) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
         }
-    )
-    if (activeBtn) {
-        activeBtn.style.fontWeight = "";
-      }
-    
-      activeBtn = document.querySelector(`#${category}-btn`);
-      activeBtn.style.fontWeight = "bold";
+    }
 }
 
-let backBtn = document.querySelector("#backend-btn")
-backBtn.addEventListener("click",function(){
-    filtroCards("backend")
-})
-let frontBtn = document.querySelector("#frontend-btn")
-frontBtn.addEventListener("click",function(){
-    filtroCards("frontend")
-})
-let allBtn = document.querySelector("#all-btn")
-allBtn.addEventListener("click",function(){
-    filtroCards("all")
-})
+// select filters
+let filters = document.querySelectorAll(".filter-btn");
+
+// add click event listeners to filters
+for (let filter of filters) {
+  filter.addEventListener("click", function() {
+    // remove the "active" class from all filters
+    for (let f of filters) {
+      f.classList.remove("active");
+    }
+
+    // add the "active" class to the clicked filter
+    this.classList.add("active");
+
+    // get the selected categories
+    let selectedCategories = [...filters].filter(f => f.classList.contains("active")).map(f => f.dataset.category);
+
+    // filter the cards
+    filterCards(selectedCategories);
+  });
+}
 
